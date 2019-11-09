8128b0d17f47befd3c27a9da0f66f5055b20fb09
itayzach <zitay.zach@gmail.com>
2019-11-09 11:57:43 +0200

    changed syms hermiteH to hermite. moved tabbed plots to figures and saved them to figures dir

--- ExponentEigenfunctions.m
+++ ExponentEigenfunctions.m
@@ -1,6 +1,11 @@
 %% Restart run
 clear; close all; clc;
 rng(0); % set seed
+
+if ~exist('figures', 'dir')
+    mkdir('figures')
+end
+
 %% Parameters
 % kernel
 a = 1;
@@ -21,19 +26,12 @@ b_plotEigenFigs = true;
 dx = 0.01;
 x = -10:dx:10-dx;
 
-if b_plotEigenFigs
-    if ~exist('fig', 'var')
-        fig = figure;
-        tg = uitabgroup; % tabgroup
-    end
-    thistab = uitab(tg, 'Title', 'Eigenfunctions'); % build tab
-    axes('Parent',thistab); % somewhere to plot
-end
 for m = 0:M-1
     [vPhi_m_x, ~] = SqExpEig(a, b, m, x);
     vP_x = p(x,sigma);
     
     if b_plotEigenFigs
+        fig1 = figure(1);
         if m <= 2
             if m == 0
                 plot(x, vP_x, 'DisplayName', '$p(x)$');
@@ -52,8 +50,8 @@ for m = 0:M-1
         rhs = lambda_m * phi_m_y;
         lhs = zeros(1, length(y));
         for j = 1:length(y)
-            vKernel_x = kernel(x,y(j),l);
-            lhs(j) = sum(vKernel_x.*vPhi_m_x.*vP_x*dx);
+            vKernel_y_x = kernel(y(j),x,l);
+            lhs(j) = sum(vKernel_y_x.*vPhi_m_x.*vP_x*dx);
         end
         fprintf('m = %d\n', m)
         fprintf('lambda * phi = ')
@@ -66,8 +64,9 @@ for m = 0:M-1
 end
 if b_plotEigenFigs
     hold off
-    title('Eigenfunctions')
-    legend('Interpreter', 'latex', 'FontSize', 12, 'Location', 'best')
+%     title('Eigenfunctions')
+    legend('Interpreter', 'latex', 'FontSize', 12, 'Location', 'northwest')
+    print(fig1, ['figures' filesep 'fig1_eigenfunctions'], '-dpdf')
 end
 
 %% Read mnist data
@@ -85,53 +84,64 @@ end
 % end
 
 %% Extrapolate functions
-% N = 1000;
-% x = linspace(-1, 1, N)'; % [x_min x_max] should be small since gaussians decay to zero
-
-dx = 0.002;
-x = (-1:dx:1-dx)';
+dx = 0.01;
+x = (0:dx:1-dx)';
 N = length(x);
 
 dt = 0.01;
-t = -10:dt:10-dt;
+t = x; %(-20:dt:20-dt)';
 vP_t = p(t,sigma);
 
-f = [sin(5*x) exp(-x).*sin(2.5*x) exp(-2*x).*sin(5*x)];
-nFuncs = size(f, 2);
+mF    = [sin(5*x) + exp(-x).*sin(2.5*x) exp(-2*x).*sin(5*x)];
+cFstr = {'\sin(5x)' 'e^{-x}\sin(2.5x)' 'e^{-2x}\sin(5x)'};
+nFuncs = size(mF, 2);
 
-if ~exist('fig', 'var')
-    fig = figure;
-    tg = uitabgroup; % tabgroup
-end
+% x_eval = ([-5:0.1:5])';
+% vF_eval = [sin(5*x_eval) exp(-x_eval).*sin(2.5*x_eval) exp(-2*x_eval).*sin(5*x_eval)];
+
+cFigs = cell(1, nFuncs);
+
 for i = 1:nFuncs
-    fi = f(:, i);
+    vFi = mF(:, i);
     mPhi = zeros(N, M);
     for m = 0:M-1 
-        [vPhi_m_x, ~] = SqExpEig(a, b, m, x);
+        [vPhi_m_x, lambda_m] = SqExpEig(a, b, m, x);
         mPhi(:, m+1) = vPhi_m_x;
     end
     r = 0.1*N;
-    R = randperm(N,N/r);%    1:r:N;
-    vCR = pinv(mPhi(R, :)) * fi(R);
-    fi_hat = mPhi * vCR;
+    vR = randperm(N,N/r); % 1:r:N;
+    vCR = pinv(mPhi(vR, :)) * vFi(vR);
+    vFi_hat = mPhi * vCR;
     
-%     for m = 0:M-1 
-%         for j = 1:length(x)
-%             vKernel_t = kernel(t,x(j),l);
-%             [vPhi_m_t, ~] = SqExpEig(a, b, m, t);
-%             phi_m_x(j) = sum(vKernel_t.*vPhi_m_t.*vP_t*dt);
+%     % ---------------------------------------------------------------------
+%     % Kernel try
+%     vFi_eval = vF_eval(:, i);
+%     vPhi_m_x = zeros(M, 1);
+%     vPhi_m_x2 = zeros(M, 1);
+%     vFi_hat2 = zeros(length(x_eval), 1);
+%     for j = 1:length(x_eval)
+%         for m = 0:M-1
+%             vKernel_x_t = kernel(x_eval(j),t,l);
+%             [vPhi_m_t, lambda_m] = SqExpEig(a, b, m, t);
+%             I = conv(vKernel_x_t, vPhi_m_t.*vP_t*dt, 'same');
+%             [ ~, idx ] = min( abs( t-x_eval(j) ) );
+%             vPhi_m_x2(m+1) = (1/lambda_m)*I(idx);
+%             vPhi_m_x(m+1) = (1/lambda_m)*sum(vKernel_x_t.*vPhi_m_t.*vP_t*dt);
 %         end
+%         vFi_hat2(j) = sum(vCR.*vPhi_m_x2);
+%         res = sin(5*x_eval(j));
 %     end
-    
-    thistab = uitab(tg, 'Title', ['Extrapolate f' num2str(i)]);
-    axes('Parent',thistab);
-    p1 = plot(x, fi_hat, 'LineWidth', 2, 'DisplayName', ['$f_' num2str(i) ' = \Phi c $ with ' num2str(N/r) ' points']);
-    title(['Extrapolate $f_' num2str(i) '$'], 'Interpreter', 'latex', 'FontSize', 12)
+%     % ---------------------------------------------------------------------
+    cFigs{i} = figure(i+1);
+    p1 = plot(x, vFi_hat, 'LineWidth', 2, 'DisplayName', ['$f_' num2str(i) ' = \Phi c $ with ' num2str(N/r) ' points']);
     hold on
-    p2 = plot(x, fi, '-.', 'DisplayName', ['$f_' num2str(i) '$']);
-    p3 = plot(x(R), fi(R), 'o');
+%     title(['Extrapolate $f_' num2str(i) ' = ' cFstr{i} '$'], 'Interpreter', 'latex', 'FontSize', 12)
+%     p2 = plot(x_eval, vFi_hat2, '*');
+    p3 = plot(x, vFi, '-.', 'DisplayName', ['$f_' num2str(i) ' = ' cFstr{i} '$']);
+    p4 = plot(x(vR), vFi(vR), 'o');
     hold off
-    legend([p1 p2], 'Interpreter', 'latex', 'FontSize', 12, 'Location', 'best')
+    legend([p1 p3], 'Interpreter', 'latex', 'FontSize', 12, 'Location', 'northwest')
+    print(cFigs{i}, ['figures' filesep 'fig' num2str(i+1) '_extrapolate_f' num2str(i) '.pdf'], '-dpdf')
 end
 
 
@@ -147,7 +157,8 @@ B = b/A;
 lambda_m = sqrt(2*a/A) * B^m;
 
 % m-th eigenfunction
-vHm = hermiteH(m, sqrt(2*c)*x);
+% vHm = hermiteH(m, sqrt(2*c)*x);
+vHm = hermite(m, sqrt(2*c)*x);
 vPhi_m = exp( -(c-a)*x.^2 ) .* vHm;
 end
 

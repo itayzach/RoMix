f94ea906df85db05f76cf9455c7d48a3fbb3f367
itayzach <zitay.zach@gmail.com>
2019-11-09 20:14:45 +0200

    added awgn - now we need fewer eigenfunctions?

--- ExponentEigenfunctions.m
+++ ExponentEigenfunctions.m
@@ -16,50 +16,52 @@ sigma = 1/sqrt(4*a);
 l = 1/sqrt(2*b);
 
 % num of eigenfunctions
-M = 10;
+M = 7;
 
 % simulation
-b_verifyEigenfunctions = true;
-b_plotEigenFigs = true;
+b_verifyEigenfunctions = false;
+b_plotEigenFigs = false;
 
 %% Check eigenfunctions
 dx = 0.01;
 x = -10:dx:10-dx;
 
-for m = 0:M-1
-    [vPhi_m_x, ~] = SqExpEig(a, b, m, x);
-    vP_x = p(x,sigma);
-    
-    if b_plotEigenFigs
-        fig1 = figure(1);
-        if m <= 2
-            if m == 0
-                plot(x, vP_x, 'DisplayName', '$p(x)$');
-                xlim([-5 5])
-                hold on
-            end
-            plot(x, vPhi_m_x, 'DisplayName', [ '$\phi_' num2str(m) '(x)$' ]);
-            xlim([-5 5])
-            xlabel('$x$', 'Interpreter', 'latex', 'FontSize', 14)
-        end
-    end
+if b_verifyEigenfunctions || b_plotEigenFigs
+    for m = 0:M-1
+        [vPhi_m_x, ~] = SqExpEig(a, b, m, x);
+        vP_x = p(x,sigma);
 
-    if b_verifyEigenfunctions
-        y = -2:0.5:2;
-        [phi_m_y, lambda_m] = SqExpEig(a, b, m, y);
-        rhs = lambda_m * phi_m_y;
-        lhs = zeros(1, length(y));
-        for j = 1:length(y)
-            vKernel_y_x = kernel(y(j),x,l);
-            lhs(j) = sum(vKernel_y_x.*vPhi_m_x.*vP_x*dx);
+        if b_plotEigenFigs
+            fig1 = figure(1);
+            if m <= 2
+                if m == 0
+                    plot(x, vP_x, 'DisplayName', '$p(x)$');
+                    xlim([-5 5])
+                    hold on
+                end
+                plot(x, vPhi_m_x, 'DisplayName', [ '$\phi_' num2str(m) '(x)$' ]);
+                xlim([-5 5])
+                xlabel('$x$', 'Interpreter', 'latex', 'FontSize', 14)
+            end
         end
-        fprintf('m = %d\n', m)
-        fprintf('lambda * phi = ')
-        fprintf('%.6f  ', rhs);
-        fprintf('\n');
-        fprintf('<k, phi>     = ')
-        fprintf('%.6f  ', lhs);
-        fprintf('\n');
+
+        if b_verifyEigenfunctions
+            y = -2:0.5:2;
+            [phi_m_y, lambda_m] = SqExpEig(a, b, m, y);
+            rhs = lambda_m * phi_m_y;
+            lhs = zeros(1, length(y));
+            for j = 1:length(y)
+                vKernel_y_x = kernel(y(j),x,l);
+                lhs(j) = sum(vKernel_y_x.*vPhi_m_x.*vP_x*dx);
+            end
+            fprintf('m = %d\n', m)
+            fprintf('lambda * phi = ')
+            fprintf('%.6f  ', rhs);
+            fprintf('\n');
+            fprintf('<k, phi>     = ')
+            fprintf('%.6f  ', lhs);
+            fprintf('\n');
+        end
     end
 end
 if b_plotEigenFigs
@@ -85,15 +87,16 @@ end
 
 %% Extrapolate functions
 dx = 0.01;
-x = (0:dx:1-dx)';
+x = (-1:dx:1-dx)';
 N = length(x);
 
 dt = 0.01;
 t = x; %(-20:dt:20-dt)';
 vP_t = p(t,sigma);
 
-mF    = [sin(5*x) + exp(-x).*sin(2.5*x) exp(-2*x).*sin(5*x)];
-cFstr = {'\sin(5x) + e^{-x}\sin(2.5x)' 'e^{-2x}\sin(5x)'};
+mF      = [10*sin(5*x) + 10*exp(-x).*sin(2.5*x)   10*exp(-2*x).*sin(5*x) ];
+mF_awgn = [1*randn(N,1) 1*randn(N,1)];
+cFstr = {'10\sin(5x) + 10e^{-x}\sin(2.5x)' '10e^{-2x}\sin(5x)'};
 nFuncs = size(mF, 2);
 
 % x_eval = ([-5:0.1:5])';
@@ -103,6 +106,8 @@ cFigs = cell(1, nFuncs);
 
 for i = 1:nFuncs
     vFi = mF(:, i);
+    vFi_awgn = mF_awgn(:, i);
+    vGi = vFi + vFi_awgn;
     mPhi = zeros(N, M);
     for m = 0:M-1 
         [vPhi_m_x, lambda_m] = SqExpEig(a, b, m, x);
@@ -110,7 +115,7 @@ for i = 1:nFuncs
     end
     r = 0.1*N;
     vR = randperm(N,N/r); % 1:r:N;
-    vCR = pinv(mPhi(vR, :)) * vFi(vR);
+    vCR = pinv(mPhi(vR, :)) * vGi(vR);
     vFi_hat = mPhi * vCR;
     
 %     % ---------------------------------------------------------------------
@@ -137,10 +142,11 @@ for i = 1:nFuncs
     hold on
 %     title(['Extrapolate $f_' num2str(i) ' = ' cFstr{i} '$'], 'Interpreter', 'latex', 'FontSize', 12)
 %     p2 = plot(x_eval, vFi_hat2, '*');
-    p3 = plot(x, vFi, '-.', 'DisplayName', ['$f_' num2str(i) ' = ' cFstr{i} '$']);
-    p4 = plot(x(vR), vFi(vR), 'o');
+    p3 = plot(x, vGi, '-.', 'LineWidth', 2, 'DisplayName', ['$f_' num2str(i) ' = ' cFstr{i} '$']);
+    p4 = plot(x, vFi, 'b--', 'LineWidth', 2, 'DisplayName', ['$f_' num2str(i) ' = ' cFstr{i} ' + $AWGN']);
+    p5 = plot(x(vR), vGi(vR), 'o', 'LineWidth', 2);
     hold off
-    legend([p1 p3], 'Interpreter', 'latex', 'FontSize', 12, 'Location', 'northeast')
+    legend([p1 p3 p4], 'Interpreter', 'latex', 'FontSize', 12, 'Location', 'northeast')
     print(cFigs{i}, ['figures' filesep 'fig' num2str(i+1) '_extrapolate_f' num2str(i) '.pdf'], '-dpdf')
 end
 

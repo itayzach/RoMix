41a007c962c82954bcf2e451518167dcbdc86fe2
itayzach <zitay.zach@gmail.com>
2020-02-15 13:58:48 +0200

    2-D WIP

--- GaussianExtrapolation/Extrapolate1D.m
+++ GaussianExtrapolation/Extrapolate1D.m
@@ -1,26 +1,23 @@
-function [] = Extrapolate(sParams, sSimParams)
+function [] = Extrapolate1D(sParams, sSimParams)
 
+assert(sParams.M <= sParams.R, 'You cannot have less points than eigenfunctions!');
+assert(sParams.dim == 1, 'This function works only for 1-D')
 dx = 0.01;
 x = (-5:dx:5-dx)';
-% x = (50:dx:60-dx)';
-x = repmat(x, 1, sParams.dim);
-assert(size(x, 2) == sParams.dim)
 N = length(x);
 
-
-
 A1 = 5;
-B1 = 0.001;
-C1 = 0.001;
-A2 = 10;
+A6 = 0.001;
+A10 = 0.001;
+B1 = 10;
 B2 = 7;
-C2 = 6;
+B3 = 6;
 % mF      = [ A*exp(-x).*(sin(2.5*x) + sin(2*pi*x))   A*exp(-2*x).*sin(5*x) ];
-[phi1, ~] = phi(sParams.a, sParams.b, 1, x);
-[phi2, ~] = phi(sParams.a, sParams.b, 6, x);
-[phi3, ~] = phi(sParams.a, sParams.b, 10, x);
-mF      = [ A1*phi1 + B1*phi2 + C1*phi3   ...
-            A2*exp(-0.2*x.^2).*sin(pi*x) + B2*exp(-0.5*x.^2).*sin(2*pi*x) + B2*exp(-0.3*x.^2).*sin(1*pi*x) ];
+[phi_1, ~] = phi(sParams.a, sParams.b, 1, x);
+[phi_6, ~] = phi(sParams.a, sParams.b, 6, x);
+[phi_10, ~] = phi(sParams.a, sParams.b, 10, x);
+mF      = [ A1*phi_1 + A6*phi_6 + A10*phi_10   ...
+            B1*exp(-0.2*x.^2).*sin(pi*x) + B2*exp(-0.5*x.^2).*sin(2*pi*x) + B3*exp(-0.3*x.^2).*sin(1*pi*x) ];
 mF_awgn = [sqrt(sSimParams.noiseVar1)*randn(N,1) sqrt(sSimParams.noiseVar2)*randn(N,1)];
 % cFstr   = {'10e^{-x}\big(\sin(2.5x) + \sin(2\pi x)\big)' '10e^{-2x}\sin(5x)'};
 nFuncs  = size(mF, 2);
@@ -40,7 +37,6 @@ for i = 1:nFuncs
         [vPhi_m_x, lambda_m] = phi(sParams.a, sParams.b, m, x);
         mPhi(:, m+1) = vPhi_m_x;
     end
-    assert(sParams.M <= sParams.R, 'You cannot have less points than eigenfunctions!');
     if sSimParams.b_randomStepSize
         vR = sort(randi([1 N],sParams.R,1));
     else
@@ -51,7 +47,9 @@ for i = 1:nFuncs
     
     mPhi_RM = mPhi(vR, :);
     vGR = vGi(vR);
-    vCR = pinv(mPhi_RM) * vGR;
+    vCR = (mPhi_RM.' * mPhi_RM) \ ( mPhi_RM.' * vGR );
+%     vCR = pinv(mPhi_RM) * vGR;
+    
 %     vCR = ( mPhi_RM.' * mPhi_RM + sParams.gamma*I ) \ (mPhi_RM.' * vGR);
 %     vCR = vCR.*(abs(vCR) > 1e-15);
 %     [(0:M-1)' vCR]
@@ -91,5 +89,13 @@ for i = 1:nFuncs
     legend([p2 p1 p3], 'Interpreter', 'latex', 'FontSize', 14, 'Location', 'best')
     print(cFigs{i}, [sSimParams.outputFolder filesep 'fig' num2str(i+1) '_extrapolate_f' num2str(i)], '-depsc')
     fprintf('f%d : R = %d; M = %d; SNR = %.2f; Accuracy = %.2f%%\n', i, sParams.R, sParams.M, SNR, accuracy);
+    if i == 1
+        vC = zeros(sParams.M, 1);
+        vC(1+1) = A1;
+        vC(6+1) = A6;
+        vC(10+1) = A10;
+        fprintf('    vCR       vC \n')
+        disp([vCR, vC]);
+    end
 end
 end

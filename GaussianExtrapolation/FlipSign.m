638d7bed31c0a82bc758040fcaf8bfee7bf7f967
itayzach <zitay.zach@gmail.com>
2020-08-13 20:23:46 +0300

    Now in each iteration Nystrom is done for the same data (removed the vNysRatio loop)

--- GaussianExtrapolation/FlipSign.m
+++ GaussianExtrapolation/FlipSign.m
@@ -1,11 +1,8 @@
-function [mPhiNumeric, mPhiNystrom] = FlipSign(sSimParams, mPhiAnalytic, mPhiNumeric, mPhiNystrom)
+function mPhiNumericOrNystrom = FlipSign(sSimParams, mPhiAnalytic, mPhiNumericOrNystrom)
 for m = 0:sSimParams.CalcEigenFuncsM-1
     [~, maxAnaIdx] = max(abs(mPhiAnalytic(:,m+1)));
-    if sign(mPhiAnalytic(maxAnaIdx,m+1)) ~= sign(mPhiNumeric(maxAnaIdx,m+1))
-        mPhiNumeric(:,m+1) = -mPhiNumeric(:,m+1);
-    end
-    if sign(mPhiAnalytic(maxAnaIdx,m+1)) ~= sign(mPhiNystrom(maxAnaIdx,m+1))
-        mPhiNystrom(:,m+1) = -mPhiNystrom(:,m+1);
-    end    
+    if sign(mPhiAnalytic(maxAnaIdx,m+1)) ~= sign(mPhiNumericOrNystrom(maxAnaIdx,m+1))
+        mPhiNumericOrNystrom(:,m+1) = -mPhiNumericOrNystrom(:,m+1);
+    end   
 end
 end
--- GaussianExtrapolation/PlotEigenDiffs.m
+++ GaussianExtrapolation/PlotEigenDiffs.m
@@ -32,7 +32,11 @@ end
 if ~exist(sSimParams.outputFolder, 'dir')
     mkdir(sSimParams.outputFolder)
 end
-simPrefix = strcat(sDataset.actualDataDist, num2str(sDataset.dim), 'd', '_', num2str(nysRatio*100, '%d'), 'prec');
+if isempty(nysRatio)
+    simPrefix = strcat(sDataset.actualDataDist, num2str(sDataset.dim), 'd');
+else
+    simPrefix = strcat(sDataset.actualDataDist, num2str(sDataset.dim), 'd', '_', num2str(nysRatio*100, '%d'), 'prec');
+end
 saveas(fig,strcat(sSimParams.outputFolder, filesep, simPrefix, '_eigen_diffs_m_', num2str(firstEigenIdx), '_to_', num2str(lastEigIdx)), 'epsc');
 
 end

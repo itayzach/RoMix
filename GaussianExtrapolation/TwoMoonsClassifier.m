fc241ea84f4c6f1aca891454f32b730813d743ad
itayzach <zitay.zach@gmail.com>
2020-08-25 13:51:01 +0300

    added option to load two moons dataset from Belkin's mat file

--- GaussianExtrapolation/TwoMoonsClassifier.m
+++ GaussianExtrapolation/TwoMoonsClassifier.m
@@ -12,8 +12,9 @@ nComponents = 2;
 dataDim = 2;
 nTrain = 200;
 nTest = 200;
+b_loadTwoMoonsMatFile = false;
 
-sDataset = GenerateDataset(actualDataDist, dataDim, nComponents, nTrain, nTest);
+sDataset = GenerateDataset(actualDataDist, dataDim, nComponents, nTrain, nTest, b_loadTwoMoonsMatFile);
 PlotTwoMoons(sSimParams, sDataset)
 
 %%
@@ -21,26 +22,22 @@ sDistParams = EstimateDistributionParameters(sDataset, nComponents);
 sKernelParams = GetKernelParams(sDataset, sDistParams);
 [sKernelParams.vLambdaAnaytic, sKernelParams.vComponentIndex, sKernelParams.vEigIndex] ...
                 = CalcAnalyticEigenvalues(sSimParams, sKernelParams, sDataset.dim, nComponents);
-
-
-
-
 %% LapRLS
-% gamma_A_laprls = 0.03125;
-% gamma_I_laprls = 1;
-% kernel_sigma_laprls = 1/(6*sqrt(2)); 
-% 
-% options_laprls_orig = ml_options('gamma_A', 0.1, ...
-%                      'NN',6, ...
-%                      'Kernel','rbf',...
-%                      'KernelParam',kernel_sigma_laprls, ...0.35...
-%                      'GraphWeightParam',1, ...
-%                      'GraphWeights', 'binary', ...
-%                      'GraphNormalize',true);    
-%              
-% orig_laprlsc_classifier = experiment_moon(sDataset.sData.x,sDataset.sData.y,sDataset.sData.xt,sDataset.sData.yt,'laprlsc',gamma_A_laprls,gamma_I_laprls, options_laprls_orig);
-% fprintf('---------------------------------------------------\n')
-% plot_classifier(orig_laprlsc_classifier, sSimParams.outputFolder, sDataset.sData.xt, zeros(size(sDataset.sData.yt)))
+gamma_A_laprls = 0.03125;
+gamma_I_laprls = 1;
+kernel_sigma_laprls = 1/(6*sqrt(2)); 
+
+options_laprls_orig = ml_options('gamma_A', 0.1, ...
+                     'NN',6, ...
+                     'Kernel','rbf',...
+                     'KernelParam',kernel_sigma_laprls, ...0.35...
+                     'GraphWeightParam',1, ...
+                     'GraphWeights', 'binary', ...
+                     'GraphNormalize',true);    
+             
+orig_laprlsc_classifier = experiment_moon(sDataset.sData.x,sDataset.sData.y,sDataset.sData.xt,sDataset.sData.yt,'laprlsc',gamma_A_laprls,gamma_I_laprls, options_laprls_orig);
+fprintf('---------------------------------------------------\n')
+plot_classifier(orig_laprlsc_classifier, sSimParams.outputFolder, sDataset.sData.xt, zeros(size(sDataset.sData.yt)))
 
 
 %% EigRLS params

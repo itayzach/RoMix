0fb97c83db8204f9f1a1e41ca1ecd1985ad1a5b3
itayzach <zitay.zach@gmail.com>
2020-09-23 18:50:18 +0300

    PlotSpectrum - we can infer the number of eigenvalues to plot from the length

--- GaussianExtrapolation/plot/PlotSpectrum.m
+++ GaussianExtrapolation/plot/PlotSpectrum.m
@@ -1,38 +1,38 @@
 function fig = PlotSpectrum(sSimParams, sDataset, vNysRatio, vLambdaAnalytic, vLambdaNumeric, mLambdaNystrom)
 
 fig = figure('Name', 'Spectrum');
-
+M = length(vLambdaAnalytic);
 subplot(2,1,1);
-stem(0:sSimParams.PlotSpectM-1, vLambdaAnalytic, 'x', 'DisplayName', '$\lambda^{\phi}_m$');
+stem(0:M-1, vLambdaAnalytic, 'x', 'DisplayName', '$\lambda^{\phi}_m$');
 hold on;
-stem(0:sSimParams.PlotSpectM-1, vLambdaNumeric, 'DisplayName', '$\lambda^{v}_m$');
+stem(0:M-1, vLambdaNumeric, 'DisplayName', '$\lambda^{v}_m$');
 if ~isempty(vNysRatio)
     for r = 1:length(vNysRatio)
         nysRatio = vNysRatio(r);
-        stem(0:sSimParams.PlotSpectM-1, mLambdaNystrom(r,:), '*', 'DisplayName', ['$\lambda^{\hat{v}}_m$' ' (' num2str(nysRatio*100, '%d') '\%)']);
+        stem(0:M-1, mLambdaNystrom(r,:), '*', 'DisplayName', ['$\lambda^{\hat{v}}_m$' ' (' num2str(nysRatio*100, '%d') '\%)']);
     end
 end
 legend('Interpreter', 'latex', 'FontSize', 14, 'Location', 'northeast');
 xlabel('$m$', 'Interpreter', 'latex', 'FontSize', 14)
 % ylabel('$\lambda_m$', 'Interpreter', 'latex', 'FontSize', 14)
-xlim([0 sSimParams.PlotSpectM + 8])
+xlim([0 M + 8])
 title('Eigenvalues', 'Interpreter', 'latex', 'FontSize', 14);
 set(gca,'FontSize', 14);
 
 subplot(2,1,2);
-stem(0:sSimParams.PlotSpectM-1, log(vLambdaAnalytic),'x', 'LineStyle','none', 'DisplayName', '$\log(\lambda^{\phi}_m)$');
+stem(0:M-1, log(vLambdaAnalytic),'x', 'LineStyle','none', 'DisplayName', '$\log(\lambda^{\phi}_m)$');
 hold on;
-stem(0:sSimParams.PlotSpectM-1, log(vLambdaNumeric),'LineStyle','none', 'DisplayName', '$\log(\lambda^{v}_m)$');
+stem(0:M-1, log(vLambdaNumeric),'LineStyle','none', 'DisplayName', '$\log(\lambda^{v}_m)$');
 if ~isempty(vNysRatio)
     for r = 1:length(vNysRatio)
         nysRatio = vNysRatio(r);
-        stem(0:sSimParams.PlotSpectM-1, log(mLambdaNystrom(r,:)),'*', 'LineStyle','none', 'DisplayName', ['$\log(\lambda^{\hat{v}}_m)$' ' (' num2str(nysRatio*100, '%d') '\%)']);
+        stem(0:M-1, log(mLambdaNystrom(r,:)),'*', 'LineStyle','none', 'DisplayName', ['$\log(\lambda^{\hat{v}}_m)$' ' (' num2str(nysRatio*100, '%d') '\%)']);
     end
 end
 
 legend('Interpreter', 'latex', 'FontSize', 14, 'Location', 'northeast');
 xlabel('$m$', 'Interpreter', 'latex', 'FontSize', 14)
-xlim([0 sSimParams.PlotSpectM + 8])
+xlim([0 M + 8])
 title('Eigenvalues log-scale', ...
     'Interpreter', 'latex', 'FontSize', 14);
 set(gca,'FontSize', 14);

56023cf07ccbca30e075896f49813a454a208d48
itayzach <zitay.zach@gmail.com>
2020-10-26 08:42:24 +0200

    plot now supports 1D graph

--- GaussianExtrapolation/plot/PlotGraphToGraphTransformAndBack.m
+++ GaussianExtrapolation/plot/PlotGraphToGraphTransformAndBack.m
@@ -10,7 +10,11 @@ v_tilde = G2.coords;
 figure;
 subplot(1,3,1);
     if exist('f1', 'var')
-        gsp_plot_signal(G1,f1,param);
+        if size(v,2) > 1
+            gsp_plot_signal(G1,f1,param);
+        else
+            plot(G1.coords, f1, '.');
+        end
         title(['Graph-signal on ' G1_title newline f1_title], 'Interpreter', 'latex', 'FontSize', 14);
     else
         gsp_plot_graph(G1,param);
@@ -21,16 +25,26 @@ subplot(1,3,1);
         text(v(vPlotIndexes,1) + 0.05, v(vPlotIndexes,2)+0.01, v(vPlotIndexes,3), vNumAsText,...
             'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
         scatter3(v(vPlotIndexes,1), v(vPlotIndexes,2), v(vPlotIndexes,3), 'ko')
-    else
+    elseif G_dataDim == 2
         text(v(vPlotIndexes,1) + 0.05, v(vPlotIndexes,2)+0.01, vNumAsText,...
             'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
         scatter(v(vPlotIndexes,1), v(vPlotIndexes,2), 'ko')
+    elseif G_dataDim == 1
+        text(v(vPlotIndexes) + 0.05, f1(vPlotIndexes)+0.01, vNumAsText,...
+            'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
+        scatter(v(vPlotIndexes), f1(vPlotIndexes), 'ko')
+    else
+        error('invalid dim');
     end
     view(0,90)
 
 subplot(1,3,2);
     if exist('f2', 'var')
-        gsp_plot_signal(G2,f2,param);
+        if size(v,2) > 1
+            gsp_plot_signal(G2,f2,param);
+        else
+            plot(G2.coords, f2, '.');
+        end
         title(['Graph-signal on ' G2_title  newline f2_title], 'Interpreter', 'latex', 'FontSize', 14);
     else
         gsp_plot_graph(G2,param);
@@ -41,16 +55,26 @@ subplot(1,3,2);
         text(v_tilde(vPlotIndexes,1) + 0.05, v_tilde(vPlotIndexes,2)+0.01, v_tilde(vPlotIndexes,3), vNumAsText,...
             'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
         scatter3(v_tilde(vPlotIndexes,1), v_tilde(vPlotIndexes,2), v_tilde(vPlotIndexes,3), 'ko')
-    else
+    elseif G_dataDim == 2
         text(v_tilde(vPlotIndexes,1) + 0.05, v_tilde(vPlotIndexes,2)+0.01, vNumAsText,...
             'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
         scatter(v_tilde(vPlotIndexes,1), v_tilde(vPlotIndexes,2), 'ko')
+    elseif G_dataDim == 1
+        text(v_tilde(vPlotIndexes,1) + 0.05, f2(vPlotIndexes)+0.01, vNumAsText,...
+            'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
+        scatter(v_tilde(vPlotIndexes), f2(vPlotIndexes), 'ko')
+    else
+        error('invalid dim');
     end
 subplot(1,3,3);
     if exist('f3', 'var')
-        gsp_plot_signal(G3,f3,param);
-        error = norm(f1-f3)/norm(f1);
-        title(['Graph-signal on ' G3_title  newline f3_title newline 'error = ' num2str(error)], 'Interpreter', 'latex', 'FontSize', 14);
+        if size(v,2) > 1
+            gsp_plot_signal(G3,f3,param);
+        else
+            plot(G3.coords, f3, '.');
+        end
+        err = norm(f1-f3)/norm(f1);
+        title(['Graph-signal on ' G3_title  newline f3_title newline 'error = ' num2str(err)], 'Interpreter', 'latex', 'FontSize', 14);
     else
         gsp_plot_graph(G3,param);
         title('Transformed graph', 'Interpreter', 'latex', 'FontSize', 14);
@@ -60,10 +84,16 @@ subplot(1,3,3);
         text(v(vPlotIndexes,1) + 0.05, v(vPlotIndexes,2)+0.01, v(vPlotIndexes,3), vNumAsText,...
             'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
         scatter3(v(vPlotIndexes,1), v(vPlotIndexes,2), v(vPlotIndexes,3), 'ko')
-    else
+    elseif G_dataDim == 2
         text(v(vPlotIndexes,1) + 0.05, v(vPlotIndexes,2)+0.01, vNumAsText,...
             'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
         scatter(v(vPlotIndexes,1), v(vPlotIndexes,2), 'ko')
+    elseif G_dataDim == 1
+        text(v(vPlotIndexes) + 0.05, f3(vPlotIndexes)+0.01, vNumAsText,...
+            'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
+        scatter(v(vPlotIndexes), f3(vPlotIndexes), 'ko')
+    else
+        error('invalid dim');
     end
     view(0,90)
     

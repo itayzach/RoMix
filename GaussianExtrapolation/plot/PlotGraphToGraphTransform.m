6fb83f14bf5c6e28474284ecde32ebe820912bf0
itayzach <zitay.zach@gmail.com>
2020-10-26 09:38:52 +0200

    TransformWithFmaps - works with f=V*alpha --> f_tilde=V_tilde*alpha_tilde and back

--- GaussianExtrapolation/plot/PlotGraphToGraphTransform.m
+++ GaussianExtrapolation/plot/PlotGraphToGraphTransform.m
@@ -1,50 +1,80 @@
-function [] = PlotGraphToGraphTransform(G1, G2, G1_title, G2_title, f1, f2, f1_title, f2_title)
+function [] = PlotGraphToGraphTransform(G1, G1_title, G2, G2_title, G3, G3_title, f1, f1_title, f2, f2_title, f3, f3_title)
 
 param.show_edges = false;
-vPlotIndexes = (1:5)';(1:3:20)';
-vNumAsText = strcat('\leftarrow ', {' '}, cellstr(num2str(vPlotIndexes)))';
-v = G1.coords;
-G_dataDim = size(G1.coords,2);
-G_tilde_dataDim = size(G2.coords,2);
-v_tilde = G2.coords;
-figure;
-subplot(1,2,1);
-    if exist('f1', 'var')
-        gsp_plot_signal(G1,f1,param);
-        title(['Graph-signal on ' G1_title newline f1_title], 'Interpreter', 'latex', 'FontSize', 14);
-    else
-        gsp_plot_graph(G1,param);
-        title('Original graph', 'Interpreter', 'latex', 'FontSize', 14);
-    end
-    hold on;
-    if G_dataDim == 3
-        text(v(vPlotIndexes,1) + 0.05, v(vPlotIndexes,2)+0.01, v(vPlotIndexes,3), vNumAsText,...
-            'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
-        scatter3(v(vPlotIndexes,1), v(vPlotIndexes,2), v(vPlotIndexes,3), 'ko')
-    else
-        text(v(vPlotIndexes,1) + 0.05, v(vPlotIndexes,2)+0.01, vNumAsText,...
-            'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
-        scatter(v(vPlotIndexes,1), v(vPlotIndexes,2), 'ko')
-    end
-    view(0,90)
 
-subplot(1,2,2);
-    if exist('f2', 'var')
-        gsp_plot_signal(G2,f2,param);
-        title(['Graph-signal on ' G2_title  newline f2_title], 'Interpreter', 'latex', 'FontSize', 14);
-    else
-        gsp_plot_graph(G2,param);
-        title('Transformed graph', 'Interpreter', 'latex', 'FontSize', 14);
+rperm = randperm(size(G1.coords,1));
+vPlotIndexes = rperm(1:5)'; %(1:5)';(1:3:20)';
+vNumAsText = strcat('\leftarrow ', {' '}, cellstr(num2str(vPlotIndexes)))';
+
+if exist('G3', 'var')
+    nGraphs = 3;
+else
+    nGraphs = 2;
+end
+
+figure;
+for iGraph = 1:nGraphs
+    if iGraph == 1
+        G = G1;
+        G_title = G1_title;
+        if exist('f1', 'var')
+            f = f1;
+            f_title = f1_title;
+        end
+        
+    elseif iGraph == 2
+        G = G2;
+        G_title = G2_title;
+        if exist('f2', 'var')
+            f = f2;
+            f_title = f2_title;
+        end
+    elseif iGraph == 3
+        G = G3;
+        G_title = G3_title;
+        if exist('f3', 'var')
+            f = f3;
+            f_title = f3_title;
+        end
     end
-    hold on;
-    if G_tilde_dataDim == 3
-        text(v_tilde(vPlotIndexes,1) + 0.05, v_tilde(vPlotIndexes,2)+0.01, v_tilde(vPlotIndexes,3), vNumAsText,...
-            'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
-        scatter3(v_tilde(vPlotIndexes,1), v_tilde(vPlotIndexes,2), v_tilde(vPlotIndexes,3), 'ko')
+    v = G.coords;
+    G_dataDim = size(G.coords,2);
+    
+    subplot(1,nGraphs,iGraph);
+    if exist(['f' num2str(iGraph)], 'var')
+        if size(v,2) > 1
+            gsp_plot_signal(G,f,param);
+        else
+            plot(G.coords, f, '.');
+        end
+        hold on;
+        if G_dataDim == 3
+            text(v(vPlotIndexes,1) + 0.05, v(vPlotIndexes,2)+0.01, v(vPlotIndexes,3), vNumAsText,...
+                'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
+            scatter3(v(vPlotIndexes,1), v(vPlotIndexes,2), v(vPlotIndexes,3), 'ko')
+        elseif G_dataDim == 2
+            text(v(vPlotIndexes,1) + 0.05, v(vPlotIndexes,2)+0.01, vNumAsText,...
+                'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
+            scatter(v(vPlotIndexes,1), v(vPlotIndexes,2), 'ko')
+        elseif G_dataDim == 1
+            text(v(vPlotIndexes) + 0.05, f(vPlotIndexes)+0.01, vNumAsText,...
+                'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
+            scatter(v(vPlotIndexes), f(vPlotIndexes), 'ko')
+        else
+            error('invalid dim');
+        end
+        view(0,90)
+        plot_title = ['Graph-signal on ' G_title newline f_title];
+        if iGraph == 3
+            err = norm(f1-f3)/norm(f1);
+            plot_title = strcat(plot_title, [ newline 'Error = ', num2str(err, '%d')]);
+        end
+        title(plot_title, 'Interpreter', 'latex', 'FontSize', 14);
     else
-        text(v_tilde(vPlotIndexes,1) + 0.05, v_tilde(vPlotIndexes,2)+0.01, vNumAsText,...
-            'FontWeight','bold','Color', 'black', 'BackgroundColor', 'white', 'Margin', 1,'EdgeColor','black')
-        scatter(v_tilde(vPlotIndexes,1), v_tilde(vPlotIndexes,2), 'ko')
+        gsp_plot_graph(G,param);
+        title(G_title, 'Interpreter', 'latex', 'FontSize', 14);
     end
-set(gcf,'Position', [400 400 1000 400])  
+end   
+set(gcf,'Position', [200 400 nGraphs*500 400])
+
 end

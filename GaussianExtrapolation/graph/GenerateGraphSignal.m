482e7c9bfcb99853599ac61d0ff64c26c22ac707
itayzach <zitay.zach@gmail.com>
2020-11-09 14:34:01 +0200

    updated GenerateGraphSignal

--- GaussianExtrapolation/graph/GenerateGraphSignal.m
+++ GaussianExtrapolation/graph/GenerateGraphSignal.m
@@ -1,4 +1,7 @@
-function [f, f_hat, coeffs] = GenerateGraphSignal(G, mPhi, graphSignalModel)
+function [f, coeffs] = GenerateGraphSignal(B, graphSignalModel)
+
+N = size(B,1);
+
 if strcmpi(graphSignalModel, 'bandlimited') || strcmpi(graphSignalModel, 'U_fhat')
     % paramf.log = 1;
     % Nf = 5;
@@ -11,36 +14,31 @@ if strcmpi(graphSignalModel, 'bandlimited') || strcmpi(graphSignalModel, 'U_fhat
     % orig_c = [0 0 100 0 0 0 0 0 0 0]';
     % f = G.U(:,1:nEigs)*orig_c;
 
-    % K = floor(samplingRatio*G.N);
+    % K = floor(samplingRatio*N);
     % f_hat_K = 100*exp(-0.5*(1:K))';
-    % f_hat = [f_hat_K; zeros(G.N-K,1)];
+    % f_hat = [f_hat_K; zeros(N-K,1)];
     % f = G.U*(f_hat);
 
-    % f_hat = 5*exp(-0.5*(1:G.N))';
+    % f_hat = 5*exp(-0.5*(1:N))';
     % f = G.U*(f_hat);
     
-    k0 = round(0.01*G.N);
-    f_hat = zeros(G.N,1);
+    k0 = round(0.01*N);
+    f_hat = zeros(N,1);
     f_hat(1:k0) = 5*sort(abs(randn(k0,1)), 'descend');
-    f = G.U*f_hat;
+    f = B*f_hat;
     coeffs = f_hat;
 else
     if strcmpi(graphSignalModel, 'V_c') || strcmpi(graphSignalModel, 'Phi_c')
-        nEigs = size(mPhi,2);
+        nEigs = size(B,2);
         c_orig = 10*randn(nEigs, 1);
-        f = mPhi*c_orig;
+        f = B*c_orig;
         coeffs = c_orig;
     elseif strcmpi(graphSignalModel, 'K_alpha')
-        alpha = randn(G.N,1);
-        f = G.W*alpha; % K*alpha
+        alpha = randn(N,1);
+        f = B*alpha; % K*alpha
         coeffs = alpha;
     else
         error('unknown graphSignalModel');
-    end
-    if isfield(G, 'U')
-        f_hat = G.U*f;
-    else
-        f_hat = [];
     end
 end
 

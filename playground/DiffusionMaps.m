441eb4c44f78a78a78f4ab2b46124a5cee81a2e0
itayzach <zitay.zach@gmail.com>
2020-12-28 09:52:59 +0200

    DiffusionMaps - updated cmap

--- playground/DiffusionMaps.m
+++ playground/DiffusionMaps.m
@@ -6,10 +6,10 @@ set(0,'DefaultFigureWindowStyle','docked')
 b_plotEigenvectors = true;
 %% Data
 dataset = 'trefoil'; % 'torus' / 'trefoil'
-N = 1000;
+N = 3000;
 b_normalize = true;
 b_plotDataset = true;
-X = GetDiffMapsDataset(dataset, N, b_normalize, b_plotDataset);
+[X, cmap] = GetDiffMapsDataset(dataset, N, b_normalize, b_plotDataset);
 N = length(X);
 %% Kernel
 epsilon = 0.01;
@@ -26,8 +26,8 @@ q_eps = sum(K)';
 Q_eps = diag(q_eps);
 
 figure('Name', 'q');
-cmap = 1:N;
-scatter(1:N, q_eps, 50, cmap, 'filled');
+colormap('jet');
+scatter(cmap, q_eps, 50, cmap, 'filled');
 title(strcat('$q$'),...
     'interpreter','latex', 'FontSize', 16); set(gca,'FontSize', 14);
 
@@ -47,8 +47,8 @@ end
 alphaStr = strcat('($\alpha = ', num2str(alpha),':\quad$ ',alphaDesc,')');
 
 figure('Name', 'Da');
-cmap = 1:N;
-scatter(1:N, da, 50, cmap, 'filled');
+colormap('jet');
+scatter(cmap, da, 50, cmap, 'filled');
 title(['$D_\alpha, \quad$', newline ,alphaStr],...
     'interpreter','latex', 'FontSize', 16); set(gca,'FontSize', 14);
 
@@ -131,7 +131,7 @@ t = 1;
 diffmap = diag(Lambda)'.^t.*Psi;
 
 figure('Name', 'Diffusion map');
-cmap = 1:N;
+colormap('jet');
 scatter(diffmap(:,2), diffmap(:,3), 50, cmap, 'filled')
 xlabel('$\lambda^t_2 \psi^{(\alpha)}_2$','interpreter','latex')
 ylabel('$\lambda^t_3 \psi^{(\alpha)}_3$','interpreter','latex')
@@ -140,25 +140,24 @@ title(['$t = ', num2str(t),',\quad$', newline, alphaStr],...
 set(gca,'FontSize', 14);
 
 %% Dataset
-function X = GetDiffMapsDataset(dataset, N, b_normalize, b_plotDataset)
+function [X,cmap] = GetDiffMapsDataset(dataset, N, b_normalize, b_plotDataset)
     if strcmp(dataset, 'torus')
         R = 10; r = 4;
-        tmin = 0;
-        tmax = 1;
-        t = linspace(tmin,tmax,sqrt(N))';
+        t = linspace(0,1,sqrt(N))';
         [x, y] = meshgrid(t,t);
         x = x(:);
         y = y(:);
         X = [(R + r*cos(2*pi*y)).*cos(2*pi*x), (R + r*cos(2*pi*y)).*sin(2*pi*x), r*sin(2*pi*y) ];    
     elseif strcmp(dataset, 'trefoil')
-        tmin = 0;
-        tmax = 2*pi;
-        t = linspace(tmin,tmax,N+1)'; t = t(1:end-1);
-%         t = sort(2*pi*rand(N,1));
+%         t = linspace(0,2*pi,N)';
+        t = 2*pi*rand(N,1);
         X = [sin(t)+2*sin(2*t), cos(t)-2*cos(2*t), -sin(3*t) ];
     else
         error('invalid dataset')
     end
+    
+    % color-code each point by the generated parameter to see the match after the embedding
+    cmap = t;
 
     if b_normalize
         X = X - mean(X);
@@ -167,7 +166,7 @@ function X = GetDiffMapsDataset(dataset, N, b_normalize, b_plotDataset)
 
     if b_plotDataset
         figure('Name', 'Dataset');
-        cmap = 1:length(X);
+        colormap('jet');
         scatter3(X(:,1), X(:,2), X(:,3), 50, cmap, 'filled');
         title(strcat('$X$', ' (', dataset, ')'), 'interpreter', 'latex', 'FontSize', 16); 
         set(gca,'FontSize', 14);

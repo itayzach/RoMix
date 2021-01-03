ee03c98cde788ed5c9f09b20d277e9bddf17648f
itayzach <zitay.zach@gmail.com>
2021-01-03 22:50:58 +0200

    updated FlipSign

--- GaussianExtrapolation/utils/FlipSign.m
+++ GaussianExtrapolation/utils/FlipSign.m
@@ -1,14 +1,18 @@
 function mPhiNumericOrNystrom = FlipSign(mPhiAnalytic, mPhiNumericOrNystrom)
 
-M1 = size(mPhiAnalytic,2);
-M2 = size(mPhiNumericOrNystrom,2);
+[N1, M1] = size(mPhiAnalytic);
+[N2, M2] = size(mPhiNumericOrNystrom);
 M = min(M1,M2);
 
 for m = 0:M-1
     [~, maxAnaIdx] = max(abs(mPhiAnalytic(:,m+1)));
     [~, maxNumIdx] = max(abs(mPhiNumericOrNystrom(:,m+1)));
-    if sign(mPhiAnalytic(maxAnaIdx,m+1)) ~= sign(mPhiNumericOrNystrom(maxNumIdx,m+1))
+    if N1 == N2
+        if norm(mPhiAnalytic(:,m+1) - mPhiNumericOrNystrom(:,m+1)) >  norm(mPhiAnalytic(:,m+1) + mPhiNumericOrNystrom(:,m+1))
+            mPhiNumericOrNystrom(:,m+1) = -mPhiNumericOrNystrom(:,m+1);
+        end
+    elseif sign(mPhiAnalytic(maxAnaIdx,m+1)) ~= sign(mPhiNumericOrNystrom(maxNumIdx,m+1))
         mPhiNumericOrNystrom(:,m+1) = -mPhiNumericOrNystrom(:,m+1);
-    end   
+    end
 end
 end

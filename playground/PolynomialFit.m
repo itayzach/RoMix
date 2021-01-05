71a5a2773d71f026d269437da46702ff4f709691
itayzach <zitay.zach@gmail.com>
2021-01-05 18:32:40 +0200

    PolynomailFit - looks okay

--- playground/PolynomialFit.m
+++ playground/PolynomialFit.m
@@ -1,49 +1,91 @@
 %% Restart
 clc; clear; close all;
-
+rng('default')
 %% Number of points
 n = 1000;
 
-%% Uniform data
-xMax = 1;
-xMin = -1;
+%% (Source) uniform data
+% --------------------------------------------------------------------------------------------------
+% Generate data
+% --------------------------------------------------------------------------------------------------
+xMax = 4;
+xMin = -3;
 x = (xMax - xMin)*rand(n,1) + xMin;
-figure('Name', 'Histogram of Y'); 
-histogram(x,100);
-title('Histogram of $X$', 'interpreter', 'latex', 'FontSize', 16); set(gca,'FontSize', 14);
-set(gca,'FontSize', 14);
-
-[xCdf, x2] = ecdf(x);
-figure; plot(x2,xCdf);
-x2 = x2(2:end);
-xCdf = xCdf(2:end);
-p = polyfit(x2,xCdf,5);
-%% Gaussian data
-sigma = 1; mu = 0;
-xTilde = sigma*randn(n,1) + mu;
-
 figure('Name', 'Histogram of X'); 
-subplot(2,1,1)
-histogram(xTilde,100);
-title('Histogram of $X$', 'interpreter', 'latex', 'FontSize', 16); set(gca,'FontSize', 14);
-subplot(2,1,2)
-plot(xTilde,'.')
-xlabel('$i$','interpreter', 'latex')
-ylabel('$x(i)$','interpreter', 'latex')
+histogram(x,100);
+title('Histogram of $X$', 'interpreter', 'latex', 'FontSize', 16);
 set(gca,'FontSize', 14);
 
-gaussianCDF = cdf('Normal',xTilde,mu,sigma);
-figure; plot(xTilde,gaussianCDF,'.');
+% --------------------------------------------------------------------------------------------------
+% Estimate x CDF
+% --------------------------------------------------------------------------------------------------
+[estCdf, x_new] = ecdf(x);
+x_new = x_new(1:end-1);
+estCdf = estCdf(1:end-1);
+pCdf = polyfit(x_new,estCdf,5); % to have analytic expression for the cdf
+polyvalCdf = polyval(pCdf, x);
 
+% --------------------------------------------------------------------------------------------------
+% Sort x points
+% --------------------------------------------------------------------------------------------------
+[xSorted, xSortedInd] = sort(x);
+polyvalCdfSorted = polyval(pCdf, xSorted);
 
-%% Polynomial fit
-p_yCDF_to_gaussianCDF = polyfit(xCdf,gaussianCDF,7);
-pinverse = polyfit(gaussianCDF,xCdf,7);
+% --------------------------------------------------------------------------------------------------
+% Plot x estimated CDF vs. analytic x poly CDF
+% --------------------------------------------------------------------------------------------------
+figure('Name', 'Verify estCdfPoly');
+plot(x_new, estCdf,'.');
+hold on;
+plot(x, polyvalCdf, 'o');
+plot(xSorted, polyvalCdfSorted, '.');
+
+title('ecdf(x) vs. polynomial estimation of CDF(x)', 'interpreter', 'latex', 'FontSize', 16);
+legend('ecdf(x)', 'polyvalCdf(x)','polyvalCdfSorted(x)', ...
+    'location', 'southeast', 'interpreter', 'latex', 'FontSize', 14)
+set(gca,'FontSize', 14);
+
+%% (Target) Gaussian data
+% --------------------------------------------------------------------------------------------------
+% Generate data
+% --------------------------------------------------------------------------------------------------
+sigma = 5; mu = 0;
+xTilde = sigma*randn(n,1) + mu;
+figure('Name', 'Histogram of xTilde'); 
+histogram(xTilde,100);
+title('Histogram of $\tilde{X}$', 'interpreter', 'latex', 'FontSize', 16);
+set(gca,'FontSize', 14);
+
+% --------------------------------------------------------------------------------------------------
+% Sort xTilde points
+% --------------------------------------------------------------------------------------------------
+[xTildeSorted, xTildeSortedInd] = sort(xTilde);
+cdfTildeSorted = cdf('Normal',xTildeSorted,mu,sigma);
+
+% --------------------------------------------------------------------------------------------------
+% Plot Gaussian CDF
+% --------------------------------------------------------------------------------------------------
+figure('Name', 'Gaussian cdf(xTilde)'); 
+plot(xTildeSorted,cdfTildeSorted,'.');
+title('Gaussian cdf($\tilde{X}$)', 'interpreter', 'latex', 'FontSize', 16);
+set(gca,'FontSize', 14);
+
+%% Polynomial fit between CDF(x) and CDF(xTilde)
+T = polyfit(polyvalCdfSorted,cdfTildeSorted,7);
+Tinv = polyfit(cdfTildeSorted,polyvalCdfSorted,7);
 %% Plot fit
-y_interp = polyval(p, -1:0.001:1);
-figure; plot(-1:0.001:1, y_interp, '.');
+dx = 0.001;
+zeroOneAxis = (0:dx:1-dx)'; % possible values of a CDF are always [0,1]
+gaussCdfZeroOne = polyval(T, zeroOneAxis);
 
-gaussianCDF_interp = polyval(p_yCDF_to_gaussianCDF, y_interp);
-gaussianNodes_interp = icdf('Normal',gaussianCDF_interp,mu,sigma);
+figure('Name', 'Polyval(T,[0,1])');
+plot(zeroOneAxis,gaussCdfZeroOne,'.')
+title('polyval(T, [0,1]) - should be Gaussian cdf', 'interpreter', 'latex', 'FontSize', 16);
+set(gca,'FontSize', 14);
 
-figure; histogram(gaussianNodes_interp);
+icdfZeroOne = icdf('Normal',gaussCdfZeroOne,mu,sigma);
+
+figure('Name', 'icdf([0,1])'); 
+histfit(icdfZeroOne,100);
+title('icdf([0,1]) - should be Gaussian pdf', 'interpreter', 'latex', 'FontSize', 16);
+set(gca,'FontSize', 14);

584b38b6a02b1a46ebaf0615681204042a578687
itayzach <zitay.zach@gmail.com>
2021-07-23 16:21:51 +0300

    EigsByType returns always the adjacency eigenvalues (for Nystrom. e.g, the first eigenvalue of L is 0...)

--- GaussianExtrapolation/calc/EigsByType.m
+++ GaussianExtrapolation/calc/EigsByType.m
@@ -1,13 +1,13 @@
-function [V, lambda] = EigsByType(W, M, matrixForEigs)
+function [V, adjLambda, matLambda] = EigsByType(W, M, matrixForEigs)
 
 if strcmp(matrixForEigs, 'Adjacency')
     [V, Lambda] = eigs(W,M);
-    lambda = diag(Lambda);
+    matLambda = diag(Lambda);
 elseif strcmp(matrixForEigs, 'RandomWalk')
 %     d = sum(W,2);
 %     S = diag(d.^-0.5)*W*diag(d.^-0.5);
 %     [V, Lambda] = eigs(S, M);
-%     lambda = diag(Lambda);
+%     matLambda = diag(Lambda);
 %     V = diag(d.^-0.5)*V;
 %     V = V./norm(V(:,1));
 
@@ -19,7 +19,7 @@ elseif strcmp(matrixForEigs, 'RandomWalk')
     Pa = Da^-1*Ka;
     isalmostequal(diag(sum(Pa,2)),eye(length(Pa)),1e-14) % make sure Pa is row stochastic
     [Psi, Lambda, Phi] = eigs(Pa,M);
-    lambda = diag(Lambda);
+    matLambda = diag(Lambda);
     V = Psi;
     % Psi = Psi./norm(Psi(:,1));
 elseif strcmp(matrixForEigs, 'NormLap')
@@ -29,21 +29,24 @@ elseif strcmp(matrixForEigs, 'NormLap')
     Ln = I - Wn;
     [V, Lambda] = eig(Ln);
     V = V(:,1:M);
-    lambda = diag(Lambda);
-    lambda = lambda(1:M);
+    matLambda = diag(Lambda);
+    matLambda = matLambda(1:M);
 elseif strcmp(matrixForEigs, 'Laplacian')
     d = sum(W,2);
     D = diag(d);
     L = D - W;
     [V, Lambda] = eig(L);
     V = V(:,1:M);
-    lambda = diag(Lambda);
-    lambda = lambda(1:M);
+    matLambda = diag(Lambda);
+    matLambda = matLambda(1:M);
 
 else
     error('invalid matrixType = %s', matrixForEigs);
 end
-assert(~any(isnan(lambda)), 'You have at least one NaN');
+
+adjLambda = eigs(W,M);
+
+assert(~any(isnan(adjLambda)), 'You have at least one NaN');
 
 end
 

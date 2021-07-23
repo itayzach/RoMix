3fc226a59e8aa4b6f7158902b8fa8677b9a67a86
itayzach <zitay.zach@gmail.com>
2021-07-23 16:05:39 +0300

    GetPlotParams - added

---
+++ GaussianExtrapolation/plot/GetPlotParams.m
@@ -0,0 +1,15 @@
+function sPlotParams = GetPlotParams()
+% sPlotParams.outputFolder = 'figs';
+sPlotParams.b_plotAllEvecs             = false;
+sPlotParams.b_GSPBoxPlots              = false;
+sPlotParams.b_plotWeights              = true;
+sPlotParams.b_plotVRec                 = false;
+sPlotParams.b_plotTransDemos           = false;
+sPlotParams.b_plotOrigVsInterpEvecs    = true;
+sPlotParams.b_plotTildeFiguresForDebug = false;
+sPlotParams.b_plotData                 = false;
+sPlotParams.b_plotDataVsGmm            = true;
+sPlotParams.b_plotInnerProductMatrices = true;
+sPlotParams.b_plotHistogram            = false;
+sPlotParams.b_plotC                    = true;
+end

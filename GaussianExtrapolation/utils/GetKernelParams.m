e26707d5cc3c68bacace9dc7d131aa8353ab5899
itayzach <zitay.zach@gmail.com>
2021-12-09 20:05:35 +0200

    GetKernelParams - beautified prints

--- GaussianExtrapolation/utils/GetKernelParams.m
+++ GaussianExtrapolation/utils/GetKernelParams.m
@@ -4,23 +4,32 @@ dim = sDistParams.dim;
 if strcmp(sDistParams.estDataDist, 'Gaussian')
     sKernelParams.kernelType = 'gaussian';
     %% second type consts
+    fprintf('*********************************************************\n');
     fprintf('*            Using beta,omega constants                 *\n');
     fprintf('*********************************************************\n');
     sKernelParams.omega = omega;
     sKernelParams.t = 0.5*sKernelParams.omega^2;
+    fprintf('omega    (kernel width)            = %8.3f\n', sKernelParams.omega);
+    fprintf('---------------------------------------------------------\n');
     for c = 1:sDistParams.estNumComponents
         sKernelParams.beta{c} = 2*sDistParams.sigma{c}.^2/sKernelParams.omega^2;
-
+        fprintf('mu(%d)    (pdf mean)                = [',c);
         for d = 1:dim
-            fprintf('sigma(%d) (pdf width)     = %8.3f\n', d, sDistParams.sigma{c}(d));
-            fprintf('mu(%d)    (pdf mean)      = %8.3f\n', d, sDistParams.mu{c}(d));
+            fprintf('%8.3f, ', sDistParams.mu{c}(d)); 
         end
-            fprintf('omega    (kernel width)  = %8.3f\n', sKernelParams.omega);
+        fprintf(']\n');
+        fprintf('sigma(%d) (pdf width)               = [',c);
         for d = 1:dim
-            fprintf('--> beta(%d)              = %8.3f\n', d, sKernelParams.beta{c}(d));
+            fprintf('%8.3f, ', sDistParams.sigma{c}(d)); 
         end
+        fprintf(']\n');
+        fprintf('--> beta(%d) = 2*sigma(%d)^2/omega^2 = [', c, c);
+        for d = 1:dim
+            fprintf('%8.3f, ', sKernelParams.beta{c}(d));
+        end
+        fprintf(']\n');
+        fprintf('---------------------------------------------------------\n');
     end
-    fprintf('*********************************************************\n');
 elseif strcmp(sDistParams.estDataDist, 'uniform')
     sKernelParams.kernelType = 'sinc';
 else

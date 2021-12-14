b667a0da94e55f400fdbf25491ae8e0d6f326e4d
itayzach <zitay.zach@gmail.com>
2021-12-14 11:21:46 +0200

    EigsByType cleanup

--- GaussianExtrapolation/calc/EigsByType.m
+++ GaussianExtrapolation/calc/EigsByType.m
@@ -4,25 +4,32 @@ if strcmp(matrixForEigs, 'Adjacency')
     [V, Lambda] = eigs(W,M);
     matLambda = diag(Lambda);
     adjLambda = matLambda;
-elseif strcmp(matrixForEigs, 'RandomWalk')
-%     d = diag(D);
-%     S = diag(d.^-0.5)*W*diag(d.^-0.5);
-%     [V, Lambda] = eigs(S, M);
-%     matLambda = diag(Lambda);
-%     V = diag(d.^-0.5)*V;
-%     V = V./norm(V(:,1));
-
+elseif strcmp(matrixForEigs, 'RandomWalkDsvila')
     d = diag(D);
-    alpha = 0;
-    Ka = diag(d.^-alpha)*W*diag(d.^-alpha);
-    da = sum(Ka,2);
-    Da = diag(da);
-    Pa = Da^-1*Ka;
-    isalmostequal(diag(sum(Pa,2)),eye(length(Pa)),1e-14) % make sure Pa is row stochastic
+    S = diag(d.^-0.5)*W*diag(d.^-0.5);
+    [V, Lambda] = eigs(S, M);
+    matLambda = diag(Lambda);
+    V = diag(d.^-0.5)*V;
+    V = V./norm(V(:,1));
+    
+    adjLambda = eigs(W,M);
+elseif strcmp(matrixForEigs, 'RandomWalk')
     % Reminder: the right and left eigenvectors of a non-symmetric matrix are not equal
     %   [Psi, Lambda, Phi] = eig(Pa) ---> 
     %       Pa*psi = lambda*psi       (Single column right eigenvector)
     %       phi^T*Pa = lambda*phi^T   (Single row left eigenvector)
+    d = diag(D);
+    alpha = 0;
+    if alpha == 0
+        Pa = D^-1*W;
+    else
+        Ka = diag(d.^-alpha)*W*diag(d.^-alpha);
+        da = sum(Ka,2);
+        Da = diag(da);
+        Pa = Da^-1*Ka;
+    end
+    
+    isalmostequal(diag(sum(Pa,2)),eye(length(Pa)),1e-13) % make sure Pa is row stochastic
     
     % Take the right eigenvectors of Pa
     [Psi, Lambda] = eigs(Pa, M); 

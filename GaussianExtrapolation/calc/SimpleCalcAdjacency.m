6f7418b97292fed6a377c4fff56568c1418fbc09
itayzach <zitay.zach@gmail.com>
2021-12-14 11:09:39 +0200

    SimpleCalcAdjacency cleanups and connectivity test update

--- GaussianExtrapolation/calc/SimpleCalcAdjacency.m
+++ GaussianExtrapolation/calc/SimpleCalcAdjacency.m
@@ -1,23 +1,23 @@
 function [W, dist, D, DsqrtInv] = SimpleCalcAdjacency(xTrain, adjacencyType, distType, omega, k, nnValue)
 if strcmp(adjacencyType, 'GaussianKernel')
+    n = length(xTrain);
     dist = CalcDistance(xTrain, xTrain, distType);
     W = exp(-dist.^2/(2*omega^2));
-%     W = W - eye(length(W));
-%     W(W<0.999) = 0;
-%     W = W/W(1,2);
     epsilon = 2*omega^2;
-    distForAssert = dist;
-    distForAssert(1:length(xTrain):end) = inf;
-    assert(epsilon > max(min(distForAssert.^2)), ...
-        'Your graph is not connected. Your omega = %.2f. Consider omega > %.2f\n', ...
-        omega, 2*max(min(distForAssert.^2))^2)
+    
+    distWithoutDiag = dist;
+    distWithoutDiag(1:n+1:end) = [];
+    distWithoutDiag = reshape(distWithoutDiag,n-1,n);
+    minDist = min(distWithoutDiag);
+    recEps = (1/n)*sum(minDist.^2);
+    recOmega = 2*recEps^2;
+    assert(epsilon > max(min(distWithoutDiag.^2)), ...
+        strcat('Your graph is not fully connected, your omega = %.2f is too small. ',...
+            'Consider a bigger omega, like omega > %.2f\n'), ...
+            omega, 2*max(min(distWithoutDiag.^2))^2)
 elseif strcmp(adjacencyType, 'NearestNeighbor')
     dist = []; % irrelevant for W
     W = NearestNeighborsAdjacency(xTrain, k, nnValue);
-%     W(1,k) = 0;
-%     W(k,1) = 0;
-%     W(end,end-k+1) = 0;
-%     W(end-k+1,end) = 0;
 end
 
 d = sum(W,2);
